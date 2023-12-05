ca8464b31ebd6908b307a4a0cb4d2d012dd7fe5b
George Pittas <fozuzip@gmail.com>
2023-12-05 18:01:41 +0200

    Extend theme to fit project requirements

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-cyan text-3xl font-semibold underline">Hello world!</h1>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+      colors: {
+        cyan: "hsl(180, 62%, 55%)",
+        "very-dark-blue": "hsl(180, 62%, 55%)",
+        "grayish-blue": "hsl(229, 6%, 66%)",
+        "very-light-gray": "hsl(0, 0%, 98%)",
+      },
+      fontSize: {
+        base: ["15px", "24px"],
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
+      },
+    },
   },
   plugins: [],
 };
